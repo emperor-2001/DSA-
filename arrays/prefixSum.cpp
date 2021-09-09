e98174d0dd770b74ed69b635a4636bc7050889a2
emperor-2001 <dhirendrasingh1056@gmail.com>
2021-09-09 19:20:38 +0530

    prefix Sum approach for subarray sum

---
+++ arrays/prefixSum.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+int mpow(int base, int exp); 
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+//=======================
+
+int prefixSubarraySum(int arr[],int n)
+{
+    int prefixSum[100] ={0};
+    prefixSum[0]=arr[0];
+
+    Fo(i,1,n)
+    prefixSum[i] = arr[i] + prefixSum[i-1];
+
+   int largest=0;
+
+    fo(i,n) 
+    Fo(j,i,n)
+    {
+        int c=  i > 0 ? prefixSum[j]-prefixSum[i-1] : prefixSum[j];
+   largest = max(largest,c);
+    }
+   return largest;
+    
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int arr[] ={ -2,3,4,-1,5,-12,6,1,3};
+    int n = sizeof(arr)/sizeof(int);
+
+    cout << "largest subarray sum =  " << prefixSubarraySum(arr,n) << endl;
+
+
+
+    return 0;
+}
+
