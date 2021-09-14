a386bc32b9cf6829e4431e949b2094425ae25750
emperor-2001 <dhirendrasingh1056@gmail.com>
2021-09-14 20:05:01 +0530

    2D array key search

---
+++ 2D_Array/StaircaseSearch.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define el  "\n"
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+int mpow(int base, int exp); 
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+//=======================
+
+//vi g[N];
+//int a[N];
+
+bool staircase(vector<vector<int>>&v, int key)
+{
+
+    int i=0,j=v[0].size()-1;
+ 
+   while(1) 
+   {
+       if(key < v[i][j])
+         {
+                j--;
+
+                 if(j < 0)
+              return false;
+         }
+             
+
+       else if(key > v[i][j])
+           {
+               i++;
+
+              if(i >= v.size())
+                return false;
+              
+           }
+
+        else
+          return true;   
+             
+         
+   }
+
+   return false;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+  int n,m,x;
+   vector<vector<int>>matrix;
+   cin >> n >> m;
+
+   for(int i=0;i<n;i++)
+    {
+        vector<int>v;
+        for(int i=0;i<m;i++)
+        {
+          cin >> x;
+          v.push_back(x) ; 
+        }
+        matrix.push_back(v);   
+    }
+
+ 
+ int key;
+ cin >> key;
+
+   
+
+   if(staircase(matrix,key))
+   cout << "key found" << el;
+
+   else
+   cout << "key not found " << el; 
+
+
+    return 0;
+}
+
