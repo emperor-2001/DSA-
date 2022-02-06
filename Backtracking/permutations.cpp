9101b1bee01d8f051d2c6552fb210bcd414272a2
emperor-2001 <dhirendrasingh1056@gmail.com>
2022-02-06 17:52:27 +0530

    Permutations.cpp

---
+++ Backtracking/permutations.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define el  "\n"
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+int mpow(int base, int exp); 
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+//=======================
+
+//vi g[N];
+//int a[N];
+void swapp(char * c,  char * d)
+{
+    char temp;
+    temp = *c;
+    *c = *d;
+    *d = temp;
+
+    return;
+}
+
+void permutations(char str[],int n,string s)
+{
+    
+    if(n==0)
+    {
+       cout << s << el;
+       return;
+    }
+// deb(str[0]);
+    fo(i,n)
+       {
+           
+           s.push_back(str[i]);
+           char str1[10];
+           strcpy(str1,str);
+           swapp(&str1[0],&str1[i]);
+           permutations(str1+1,n-1,s);
+           s.erase(s.size()-1,1);
+       }
+     
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+   char str[10] = "abcd";
+  
+
+   int n = 4;
+  string s = "";
+
+
+  string so = "ghuj";
+  cout << so << " gh " << so+1 << el;
+//    cout << "char= "<< str[0] << el;
+    permutations(str,n,s);
+
+    return 0;
+}
+
